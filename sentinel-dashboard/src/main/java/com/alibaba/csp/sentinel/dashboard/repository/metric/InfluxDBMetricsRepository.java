44a9d26d99b50babf9a74343bce4db7ce6f61023
huangbangjing <huangbangjing@souche.com>
2021-01-19 15:41:38 +0800

    feature 由于influxdb时区为UTC，查询插入语句调整时区为UTC

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/repository/metric/InfluxDBMetricsRepository.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/repository/metric/InfluxDBMetricsRepository.java
@@ -4,7 +4,7 @@ import com.alibaba.csp.sentinel.dashboard.datasource.entity.MetricEntity;
 import com.alibaba.csp.sentinel.dashboard.datasource.entity.influxdb.MetricPO;
 import com.alibaba.csp.sentinel.dashboard.util.InfluxDBUtils;
 import com.alibaba.csp.sentinel.util.StringUtil;
-import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.influxdb.InfluxDB;
 import org.influxdb.dto.Point;
 import org.springframework.stereotype.Component;
@@ -39,7 +39,7 @@ public class InfluxDBMetricsRepository implements MetricsRepository<MetricEntity
     /**
      * 北京时间领先UTC时间8小时 UTC: Universal Time Coordinated,世界统一时间
      */
-    private static final Integer UTC_8 = 8;
+    private static final Integer UTC_8 = -8;
 
     @Override
     public void save(MetricEntity metric) {
@@ -99,8 +99,10 @@ public class InfluxDBMetricsRepository implements MetricsRepository<MetricEntity
         Map<String, Object> paramMap = new HashMap<String, Object>();
         paramMap.put("app", app);
         paramMap.put("resource", resource);
-        paramMap.put("startTime", DateFormatUtils.format(new Date(startTime), DATE_FORMAT_PATTERN));
-        paramMap.put("endTime", DateFormatUtils.format(new Date(endTime), DATE_FORMAT_PATTERN));
+        Date startTimeParam = DateUtils.addHours(new Date(startTime), UTC_8);
+        Date endTimeParam = DateUtils.addHours(new Date(endTime), UTC_8);
+        paramMap.put("startTime", startTimeParam.getTime() * 1000000);
+        paramMap.put("endTime", endTimeParam.getTime() * 1000000);
 
         List<MetricPO> metricPOList = InfluxDBUtils.queryList(SENTINEL_DATABASE, sql.toString(), paramMap, MetricPO.class);
 
@@ -115,7 +117,7 @@ public class InfluxDBMetricsRepository implements MetricsRepository<MetricEntity
         if (StringUtil.isBlank(app)) {
             return new ArrayList<>();
         }
-//        long startTime = System.currentTimeMillis() - 1000 * 60;
+        long startTimeCurrent = startTime;
         StringBuilder sql = new StringBuilder();
         sql.append("SELECT * FROM " + METRIC_MEASUREMENT);
         sql.append(" WHERE app=$app");
@@ -123,7 +125,8 @@ public class InfluxDBMetricsRepository implements MetricsRepository<MetricEntity
 
         Map<String, Object> paramMap = new HashMap<>(4);
         paramMap.put("app", app);
-        paramMap.put("startTime", DateFormatUtils.format(new Date(startTime), DATE_FORMAT_PATTERN));
+        Date startTimeParam = DateUtils.addHours(new Date(startTimeCurrent), UTC_8);
+        paramMap.put("startTime", startTimeParam.getTime() * 1000000);
 
         List<MetricPO> metricPOList = InfluxDBUtils.queryList(SENTINEL_DATABASE, sql.toString(), paramMap, MetricPO.class);
 
@@ -165,8 +168,8 @@ public class InfluxDBMetricsRepository implements MetricsRepository<MetricEntity
         MetricEntity metricEntity = new MetricEntity();
 
         metricEntity.setId(metricPO.getId());
-        metricEntity.setGmtCreate(metricPO.getGmtCreate() == null ? null : new Date(metricPO.getGmtCreate()));
-        metricEntity.setGmtModified(metricPO.getGmtModified() == null ? null : new Date(metricPO.getGmtModified()));
+        metricEntity.setGmtCreate(metricPO.getGmtCreate() == null ? null : DateUtils.addHours(new Date(metricPO.getGmtCreate()), UTC_8));
+        metricEntity.setGmtModified(metricPO.getGmtModified() == null ? null : DateUtils.addHours(new Date(metricPO.getGmtCreate()), UTC_8));
         metricEntity.setApp(metricPO.getApp());
         // 查询数据减8小时(InfluxDB使用的是UTC时间)
         metricEntity.setTimestamp(Date.from(metricPO.getTime().minusMillis(TimeUnit.HOURS.toMillis(UTC_8))));
@@ -184,12 +187,12 @@ public class InfluxDBMetricsRepository implements MetricsRepository<MetricEntity
     private void doSave(InfluxDB influxDB, MetricEntity metric) {
         influxDB.write(Point.measurement(METRIC_MEASUREMENT)
                 // 因InfluxDB默认UTC时间，按北京时间算写入数据加8小时
-//                .time(DateUtils.addHours(metric.getTimestamp(), UTC_8).getTime(), TimeUnit.MILLISECONDS)
+                .time(DateUtils.addHours(metric.getTimestamp(), UTC_8).getTime(), TimeUnit.MILLISECONDS)
                 .tag("app", metric.getApp())
                 .tag("resource", metric.getResource())
                 .addField("id", metric.getId())
-                .addField("gmtCreate", metric.getGmtCreate().getTime())
-                .addField("gmtModified", metric.getGmtModified().getTime())
+                .addField("gmtCreate", DateUtils.addHours(metric.getGmtCreate(), UTC_8).getTime())
+                .addField("gmtModified", DateUtils.addHours(metric.getGmtModified(), UTC_8).getTime())
                 .addField("passQps", metric.getPassQps())
                 .addField("successQps", metric.getSuccessQps())
                 .addField("blockQps", metric.getBlockQps())
